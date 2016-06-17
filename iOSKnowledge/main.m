344f22236d4106016995a6f78ffa552faed307d6
RoseMax <erica.correa831@gmail.com>
2016-06-17 09:56:30 -0400

    Initial commit

---
+++ iOSKnowledge/main.m
@@ -0,0 +1,35 @@
+//
+//  main.m
+//  iOSKnowledge
+//
+//  Created by Aditya Narayan on 6/17/16.
+//  Copyright © 2016 TurnToTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        //1. Zombie crash - objects that are called after they’ve been released and no longer exist.
+        
+        //2. Strong retain cycle -
+        
+        //3. Linker error -
+        
+        //4. Try/catch -
+        
+        //5. try/catch with error in try and app crashes
+        
+        //6. Stack overflow error
+        
+        //7. Method swizzling
+        
+        //8. NSDictionary to json conversion failure due to object inside dictionary
+        //Prevent these crashes by ...
+        
+        //9. When forward declaration is necessary
+           
+    }
+    return 0;
+}
